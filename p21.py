25be28703cc3d50e51c7429f05781c8ea46e8495
Khushi Trivedi <khushi.trivedi129697@marwadiuniversity.ac.in>
2026-02-18 08:21:58 +0530

    Add files via upload

---
+++ p21.py
@@ -0,0 +1,18 @@
+#for with else
+
+x=1
+for i in range(x,6):
+    if(x<5):
+        print(i)
+
+else:
+ print("not greater than 10")
+
+ #while with else
+count = 0
+while count < 5:
+    print (count, "is less than 5")
+    count = count +1
+    else:
+        print (count, "is not less than 5") 
+ 
